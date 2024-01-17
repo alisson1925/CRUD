e8fa30dc63ff8fdb48a1ba1f06a4a325532068d3
Alisson Rodrigues <alisson.r1925@gmail.com>
2024-01-16 22:19:20 -0300

    finalizado

--- CRUD/index.php
+++ CRUD/index.php
@@ -5,7 +5,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="css/bootstrap.rtl.min.css" rel="stylesheet">
-
     <title>Cadastro</title>
   </head>
   <body>
